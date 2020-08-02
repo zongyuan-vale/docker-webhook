0ad3c952a80bc7ed8fa8d89ed6132e177d84dce2
vale <zongyuan-zhang@qq.com>
2020-08-02 10:27:12 +0800

    test: test webhook

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>hello world 123</div>
+    <div>hello world. vale</div>
   </div>
 </template>
 
