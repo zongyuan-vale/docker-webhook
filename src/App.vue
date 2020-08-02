2f4721e682fa5dfd91136eccb77cf4b4c343ee22
vale <zongyuan-zhang@qq.com>
2020-08-02 21:25:12 +0800

    fix: page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>hello world. 张宗源 李小洁</div>
+    <div>张宗源 ~ 李小洁</div>
   </div>
 </template>
 
