edc829596ba0ac8aa4ef3fb937ac03337b0f6c9e
vale <zongyuan-zhang@qq.com>
2020-08-02 10:59:39 +0800

    fix: fix page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>hello world. vale</div>
+    <div>hello world. vale 123</div>
   </div>
 </template>
 
